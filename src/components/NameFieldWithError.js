97e34c99226467f889d965517d3f8f4a2f3eda63
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2023-03-21 09:40:10 +0530

    Delete src directory

--- src/components/NameFieldWithError.js
+++
@@ -1,8 +0,0 @@
-import FieldHOC from "./FieldHOC";
-import InputField from "./InputField";
-
-const NameFieldWithError = FieldHOC((props) => {
-  return <InputField {...props} />;
-});
-
-export default NameFieldWithError;
